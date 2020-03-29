7e74385069816898398b4e71de2821f5053992d7
David McMackins <contact@mcmackins.org>
2020-03-29 16:25:30 -0500

    Remove timestamp checking from markdown conversion script

--- convert.py
+++ convert.py
@@ -11,20 +11,6 @@ FOOTER_file = "FOOTER"
 input_file = argv[1] + ".md"
 output_file = argv[1] + ".html"
 
-HEADER_mtime = stat(HEADER_file)[-2]
-MDHEADER_mtime = stat(MDHEADER_file)[-2]
-FOOTER_mtime = stat(FOOTER_file)[-2]
-input_mtime = stat(input_file)[-2]
-
-skip = exists(output_file)
-
-if skip:
-    skip = not (max(HEADER_mtime, MDHEADER_mtime, FOOTER_mtime, input_mtime)
-                > stat(output_file)[-2])
-
-if skip:
-    exit(0)
-
 with open(output_file, 'wb') as o:
     call(["cat", HEADER_file], stdout=o)
     call(["markdown", MDHEADER_file], stdout=o)
